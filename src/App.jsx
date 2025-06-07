245ae1c9bddeacc685bd5ea6fb0d2670e60780c5
Abdulmalek Housroum <ahous063@uottawa.ca>
2025-06-07 15:29:56 -0400

    Initialized the react project using Vite and structured the project folders and added placeholder pages for the site.

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Booking from './pages/Booking';
+import Contact from './pages/Contact';
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
