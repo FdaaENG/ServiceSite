6d7d1b37238e0c5c2abc61d8fa485bf9214dc998
Abdulmalek Housroum <ahous063@uottawa.ca>
2025-06-07 22:49:46 -0400

    Finished up the home page.

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Home from './pages/Home';
 import Services from './pages/Services';
 import Booking from './pages/Booking';
 import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,26 @@
 function Home() {
-    return <h1>Home Page</h1>;
-  }
-  export default Home;
+  return (
+  <section className="hero-section">
+  <div className="hero-content">
+    <div className="hero-text">
+      <h1>Welcome to PawPoint</h1>
+      <p>Compassionate care for your furry family members.</p>
+      <div className="hero-actions">
+        <button>Book Appointment</button>
+        <button>Explore Services</button>
+      </div>
+    </div>
+    <footer className="hero-designer">
+      <p>
+        Designed by
+        <a href="https://abdulmalekhousroum.netlify.app" target="_blank" rel="noopener noreferrer">Abdulmalek</a>
+        &
+        <a href="https://fdaaportfolio.netlify.app" target="_blank" rel="noopener noreferrer">Fdaa</a>
+      </p>
+    </footer>
+  </div>
+  </section>
+  );
+}
+
+export default Home;
